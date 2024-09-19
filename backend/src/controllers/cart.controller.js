71d0af0c2a6853e71f15e96d7e79f5b773a9b668
unknown <asim26k@gmail.com>
2024-09-19 19:55:00 +0530

    FEATURE : Add  Product to the Cart

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -1,3 +1,35 @@
+import User from "../models/user.model.js";
+
 export const addToCart = async (req, res) => {
-  console.log("Welcome to Add Cart Route");
+  try {
+    const { productId } = req.body; // ************* Don't forget to handle the case when req comes without productId. Currentl null is getting pushe into the cartItems []. ********** //
+    const user = req.user;
+
+    const customer = await User.findById(user.userId);
+    console.log("Customer:", customer);
+
+    const existingItem = customer.cartItems.find(
+      (item) => item?.id === productId
+    ); // .find() is a method avilable on Array not to be confused with db.collection.find({})
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      customer.cartItems.push(productId);
+    }
+
+    await customer.save();
+    res.status(200).json({
+      message: "Cart updated Successfully.",
+      data: customer.cartItems,
+    });
+  } catch (error) {
+    console.log(
+      "Something went wrong while adding item to cart.",
+      error.message
+    );
+    res.status(500).json({
+      messagge: "Something went wrong on server while adding item to the cart.",
+      error: error.message,
+    });
+  }
 };
--- backend/src/routes/cart.route.js
+++ backend/src/routes/cart.route.js
@@ -5,5 +5,8 @@ import { protectRoute } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/", protectRoute, addToCart);
+// router.post("/", protectRoute, removeAllFromCart);
+// router.put("/:id", protectRoute, updateQuantity);
+// router.get("/", protectRoute, getAllProduct);
 
 export default router;
